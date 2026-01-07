74af6bb03ab36bb5deb385cc4f1a54dc3af5f979
Alexmig24 <alexandermqc2004@hotmail.com>
2026-01-07 12:42:03 -0500

    Change the port the API

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const { sequelize  } = require('./models/database');
 const autor = require('./routes/autor');
 const genero = require('./routes/genero');
